33eb9d55043f80297e1d563290c117b75876f64b
Rafael Lagisck <lagisck@hotmail.com>
2020-07-16 13:28:04 -0300

    Projeto Criado

---
+++ src/main/java/com/rafaellagisck/udemyspring/UdemyspringApplication.java
@@ -0,0 +1,13 @@
+package com.rafaellagisck.udemyspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyspringApplication.class, args);
+	}
+
+}
