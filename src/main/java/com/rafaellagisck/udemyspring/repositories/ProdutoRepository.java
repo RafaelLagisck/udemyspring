e1ede6e8adda0d03d993671d5aee38f421e69226
Rafael Lagisck <lagisck@hotmail.com>
2020-07-19 15:47:01 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/rafaellagisck/udemyspring/repositories/ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.rafaellagisck.udemyspring.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rafaellagisck.udemyspring.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+	
+	
+
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,12 @@
 INSERT INTO Categoria (nome) VALUES ('Informática');
 INSERT INTO Categoria (nome) VALUES ('Escritório');
+
+INSERT INTO Produto (nome, preco) VALUES ('Computador', 2000);
+INSERT INTO Produto (nome, preco) VALUES ('Mouse', 45);
+INSERT INTO Produto (nome, preco) VALUES ('Caneta', 10);
+
+INSERT INTO Produto_Categoria (produto_id, categoria_id) VALUES (1,1);
+INSERT INTO Produto_Categoria (produto_id, categoria_id) VALUES (1,2);
+INSERT INTO Produto_Categoria (produto_id, categoria_id) VALUES (2,2);
+INSERT INTO Produto_Categoria (produto_id, categoria_id) VALUES (3,1);
+
