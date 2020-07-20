60a01d7fa722160bea43f8df9fd1ff5ac5c9338f
Rafael Lagisck <lagisck@hotmail.com>
2020-07-20 13:08:36 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/rafaellagisck/udemyspring/services/CategoriaService.java
+++ src/main/java/com/rafaellagisck/udemyspring/services/CategoriaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafaellagisck.udemyspring.domain.Categoria;
+import com.rafaellagisck.udemyspring.services.exceptions.ObjectNotFoundException;
 import com.rafaellagisck.udemyspring.repositories.CategoriaRepository;
 
 @Service
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria buscarPorId(Integer id) {
 		Optional<Categoria> categoria = categoriaRepository.findById(id);
-		return categoria.orElse(null);
+		return categoria.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: "+ id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
