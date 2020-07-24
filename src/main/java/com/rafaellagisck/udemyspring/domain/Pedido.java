d1bfb8e6872d8eb1d9965488342aa9e79604f63e
Rafael Lagisck <lagisck@hotmail.com>
2020-07-24 16:28:18 -0300

    Atualizando para usar apenas o JSONIgnore.

--- src/main/java/com/rafaellagisck/udemyspring/domain/Pedido.java
+++ src/main/java/com/rafaellagisck/udemyspring/domain/Pedido.java
@@ -30,11 +30,9 @@ public class Pedido implements Serializable{
 	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private Date dataHoraPedido;
 	
-	@JsonManagedReference
 	@OneToOne(cascade=CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
 	
-	@JsonManagedReference
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
