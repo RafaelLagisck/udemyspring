b4fa3d61048b497cda211069c1e20ed15450bba2
Rafael Lagisck <lagisck@hotmail.com>
2020-08-14 13:45:14 -0300

    Restrinção de conteúdo: Cliente recupera apenas seus pedidos

--- src/main/java/com/rafaellagisck/udemyspring/services/PedidoService.java
+++ src/main/java/com/rafaellagisck/udemyspring/services/PedidoService.java
@@ -4,16 +4,21 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.rafaellagisck.udemyspring.domain.Cliente;
 import com.rafaellagisck.udemyspring.domain.ItemPedido;
 import com.rafaellagisck.udemyspring.domain.PagamentoComBoleto;
 import com.rafaellagisck.udemyspring.domain.Pedido;
 import com.rafaellagisck.udemyspring.domain.enums.EstadoPagamento;
-import com.rafaellagisck.udemyspring.repositories.ClienteRepository;
 import com.rafaellagisck.udemyspring.repositories.ItemPedidoRepository;
 import com.rafaellagisck.udemyspring.repositories.PagamentoRepository;
 import com.rafaellagisck.udemyspring.repositories.PedidoRepository;
+import com.rafaellagisck.udemyspring.security.UserSS;
+import com.rafaellagisck.udemyspring.services.exceptions.AuthorizationException;
 import com.rafaellagisck.udemyspring.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -70,4 +75,18 @@ public class PedidoService {
 		emailService.sendOrderConfirmationEmail(pedido);
 		return pedido;
 	}
+	
+	//Metodo para Paginação
+		public Page<Pedido> buscarPage(Integer page, Integer linesPage, String orderBy, String direction){
+			UserSS user = UserService.authenticated();
+			
+			if(user == null) {
+				throw new AuthorizationException("Acesso Negado");
+			}
+			
+			PageRequest pageRequest = PageRequest.of(page, linesPage, Direction.valueOf(direction), orderBy);
+			Cliente cliente = clienteService.buscarPorId(user.getId());
+			return pedidoRepository.findByCliente(cliente, pageRequest);
+			
+		}
 }
